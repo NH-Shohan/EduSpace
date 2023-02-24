02680b29b77c12c3b274a2c4954e85303ea29224
NH-Shohan <nh.shohan6@gmail.com>
2023-02-24 16:22:47 +0600

    Folder Created

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>EduSpace</title>
+
+  <!-- Font Link -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+
+  <!-- CSS Link -->
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="./view/components/header/header.css">
+</head>
+
+<body>
+  <?php include "view/components/header/header.php" ?>
+  <?php include "view/pages/home/home.php" ?>
+  <?php include "view/components/footer/footer.php" ?>
+</body>
+
+</html>
